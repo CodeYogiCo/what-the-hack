c5d896fcd779dda91f46cd6689a79c7fffd65073
Kunal Minda <kunal.minda@snapdeal.com>
2015-08-21 16:41:40 +0530

    Initial commit for google auth

--- src/main/java/com/snapdeal/gohack/core/IdeaController.java
+++ src/main/java/com/snapdeal/gohack/core/IdeaController.java
@@ -1,5 +1,8 @@
 package com.snapdeal.gohack.core;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +18,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson.JacksonFactory;
+
 
 
 @RestController
@@ -23,22 +32,88 @@ public class IdeaController {
 
 	@Autowired
 	private IdeaService ideaService;
+	
+	 private static final HttpTransport TRANSPORT = new NetHttpTransport();
+	 
+	 private static final JacksonFactory JSON_FACTORY = new JacksonFactory();
+	
+	GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(TRANSPORT, JSON_FACTORY)
+    .setAudience(Arrays.asList("720978887997-5tvk3foplvbv42qpa652josapujtthjj.apps.googleusercontent.com"))
+    .build();
+	
 
 
+	@RequestMapping(value="/tokensignin", method=RequestMethod.POST,headers = 
+			"content-type=application/x-www-form-urlencoded;charset=UTF-8" ,
+			produces={"application/json"},
+			consumes={"text/xml","application/json"})
+	public String googleConnect(HttpServletRequest request, @RequestParam("idtoken") String tokenId){
+		System.out.println(tokenId);
+		GoogleIdToken idToken = null;
+		Object takeme = null;
+		try {
+			idToken = verifier.verify(tokenId);
+			request.getSession().setAttribute("gtoken", true);
+			takeme = request.getSession().getAttribute("takeme");
+			request.getSession().removeAttribute("takeme");
+			System.out.println("----takeme removed from session----");
+			return (takeme==null)?"/home":takeme.toString();
+		} catch (GeneralSecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "/login";
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "/login";
+		}
+
+	}
+	
+	@RequestMapping(value="/tokensignout", method=RequestMethod.POST,headers = 
+			"content-type=application/x-www-form-urlencoded;charset=UTF-8" ,
+			produces={"application/json"},
+			consumes={"text/xml","application/json"})
+	public String googleDisconnect(HttpServletRequest request){
+		request.getSession().removeAttribute("gtoken");
+		request.getSession().removeAttribute("takeme");
+		return "/home";
+	}
+	
+	public  boolean isUserAuthorized(HttpServletRequest request){
+		return (request.getSession().getAttribute("gtoken") == null)?false: true;
+	}
+
+	public ModelAndView authorizeUser(String takeme, String failureRedirect, HttpServletRequest request ){
+		if(!isUserAuthorized(request)){
+			request.getSession().setAttribute("takeme", takeme);
+			return new ModelAndView("redirect:"+failureRedirect);
+		}
+		return null;
+	}
+	
 	@RequestMapping(value="/idea", method=RequestMethod.POST,headers = 
 			"content-type=application/x-www-form-urlencoded;charset=UTF-8" ,
 			produces={"application/json"},
 			consumes={"text/xml","application/json"})
 	public ModelAndView submitIdea(@ModelAttribute Idea idea,HttpServletRequest request){
+		ModelAndView view = authorizeUser("/ideaPage", "/login", request);
+		if(view != null){
+			return view;
+		}
 		String hostName=request.getHeader("Host");
 		String ideaNumber=ideaService.doSubmit(idea,hostName);
+
 		return new ModelAndView("redirect:/ideaDetail?idea="+ideaNumber);
 	}
 
 	@RequestMapping(value="/update", method=RequestMethod.POST,
 			produces={"application/json"},
 			consumes={"text/xml","application/json"})
-	public boolean updateIdea(@RequestBody Idea idea){
+	public boolean updateIdea(@RequestBody Idea idea, HttpServletRequest request){
+		if(!isUserAuthorized(request)){
+			return false;
+		}
 		boolean  updateStatus=ideaService.updateIdea(idea);
 		return updateStatus;
 	}
@@ -63,10 +138,17 @@ public class IdeaController {
 	}
 
 	@SuppressWarnings("unchecked")
-	@RequestMapping(value="/ideastatus/{ideaNumber}/upvote/email/{emailId}" ,method=RequestMethod.GET,produces={"application/json"})
+	@RequestMapping(value="/ideastatus/{ideaNumber}/upvote/email/{emailId}" ,method=RequestMethod.POST,produces={"application/json"})
 	public @ResponseBody Status upVote(@PathVariable("ideaNumber") String ideaNumber,
-			@PathVariable ("emailId") String emailId)
+			@PathVariable ("emailId") String emailId, HttpServletRequest request)
 	{
+		if(!isUserAuthorized(request)){
+			Status status = new Status();
+			status.setMessage("Redirect To Login");
+			status.setStatus(false);
+			request.getSession().setAttribute("takeme", "/idea/"+ideaNumber);
+			return status;
+		}
 		return ideaService.upVote(ideaNumber,emailId);
 
 
@@ -85,18 +167,29 @@ public class IdeaController {
 	}
 
 	@SuppressWarnings("rawtypes")
-	@RequestMapping(value="/ideastatus/{ideaNumber}/downvote/email/{emailId}" ,method=RequestMethod.GET)
+	@RequestMapping(value="/ideastatus/{ideaNumber}/downvote/email/{emailId}" ,method=RequestMethod.POST)
 	public @ResponseBody Status downVote(@PathVariable("ideaNumber") String ideaNumber ,
-			@PathVariable("emailId") String emailId)
+			@PathVariable("emailId") String emailId, HttpServletRequest request)
 	{
+		if(!isUserAuthorized(request)){
+			Status status = new Status();
+			status.setMessage("Redirect To Login");
+			status.setStatus(false);
+			request.getSession().setAttribute("takeme", "/idea/"+ideaNumber);
+			return status;
+		}
 		return ideaService.downVote(ideaNumber,emailId);
 
 
 	}
 
-	@RequestMapping (value="idea/{ideaNumber}/email/{emailId}",method=RequestMethod.GET)
+	@RequestMapping (value="idea/{ideaNumber}/email/{emailId}",method=RequestMethod.POST)
 	public @ResponseBody boolean collabarateIdea(@PathVariable ("emailId") String email,
-			@PathVariable ("ideaNumber") String ideaNumber){
+			@PathVariable ("ideaNumber") String ideaNumber, HttpServletRequest request){
+		if(!isUserAuthorized(request)){
+			request.getSession().setAttribute("takeme", "/idea/"+ideaNumber);
+			return false;
+		}
 		return ideaService.collabarateIdea(email,ideaNumber);
 	}
 
---
+++ src/main/resources/static/js/googleSecurity.js
@@ -0,0 +1,41 @@
+      function onSignIn(googleUser) {
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log("Name: " + profile.getName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+
+
+        var xhr = new XMLHttpRequest();
+		xhr.open('POST', 'http://localhost:8080/tokensignin');
+		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		xhr.onload = function() {
+			todo = xhr.responseText;
+		  console.log('Signed in as: ' + xhr.responseText);
+		  window.location.href = todo;
+
+		};
+		xhr.send('idtoken=' + id_token);
+
+      };
+      
+      function signOut() {
+    	    var auth2 = gapi.auth2.getAuthInstance();
+    	    auth2.signOut().then(function () {
+    	      console.log('User signed out.');
+    	      var xhr = new XMLHttpRequest();
+    			xhr.open('POST', 'http://localhost:8080/tokensignout');
+    			xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    			xhr.onload = function() {
+    				todo = xhr.responseText;
+    			  console.log('Signed in as: ' + xhr.responseText);
+    			  window.location.href = todo;
+
+    			};
+    			xhr.send();
+    	    });
+    	  }	
